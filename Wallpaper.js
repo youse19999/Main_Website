9d0eeabe6611108da5af9500e38432af883cb858
えびもなか <74030378+youse19999@users.noreply.github.com>
2022-10-07 18:01:27 +0900

    Add files via upload

---
+++ Wallpaper.js
@@ -0,0 +1,4 @@
+function Update(){
+	
+}
+setInterval(Update, 90)
--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+<<<<<<< HEAD
+var logoblur = 15;
+var blurchangespeed = 25
+function changeblurlogo(blurpx){
+    document.querySelector("html").style.filter = "blur(" + blurpx + "px)"
+    if(logoblur >= 0){
+        logoblur -= 1;
+        //console.log("blur change to " + logoblur);
+        setTimeout(changeblurlogo,blurchangespeed,logoblur);
+    }
+
+}
+function HSite(){
+	document.location = "HHH.html";
+}
+function GoWallpaper(){
+	document.location = "Wallpaper.html"
+}
+function play(){
+    playlogoblur();
+}
+function playlogoblur(){
+    setTimeout(changeblurlogo,blurchangespeed,logoblur);
+}
+function GoSims(){
+    document.location = "Sims.html";
+}
+function GoAbout(){
+    document.location = "About.html";
+}
+function GoMods(){
+    document.location = "Mods.html";
+}
+function GoIndex(){
+    document.location = "index.html";
+}
+function GoModTheSims(){
+    var res = confirm("外部サイトに移動していいですか？");
+    if(res == true){
+        document.location = "https://modthesims.info/member.php?u=10264439";
+    }
+}
+function GoGithub(){
+    var res = confirm("外部サイトに移動していいですか？");
+    if(res == true){
+        document.location = "https://modthesims.info/member.php?u=10264439";
+    }
+=======
 var logoblur = 15;
 var blurchangespeed = 25
 function changeblurlogo(blurpx){
@@ -38,4 +86,5 @@ function GoGithub(){
     if(res == true){
         document.location = "https://modthesims.info/member.php?u=10264439";
     }
+>>>>>>> e0c91f5f78bae88498ab08f8be975ee48be96af6
 }
